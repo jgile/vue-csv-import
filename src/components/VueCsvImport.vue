f747a21f9c19c10da04003b1e89592a6fd0b311a
Jon Cheng <jon.syndicate@gmail.com>
2020-04-24 11:33:09 -0400

    cleanup

--- src/components/VueCsvImport.vue
+++ src/components/VueCsvImport.vue
@@ -258,15 +258,15 @@
                 }
             },
             sample(newVal, oldVal) {
-                //auto match things in map to the sample
-                console.log(newVal, oldVal)
-                this.fieldsToMap.forEach(field => {
-                    newVal[0].forEach((columnName, index) => {
-                        if(field.key === columnName){
-                            this.map[field.key] = index;
-                        }
-                    })
-                })
+                if(newVal !== null){
+                    this.fieldsToMap.forEach(field => {
+                        newVal[0].forEach((columnName, index) => {
+                            if(field.key === columnName){
+                                this.map[field.key] = index;
+                            }
+                        });
+                    });
+                }
             }
         },
         computed: {
