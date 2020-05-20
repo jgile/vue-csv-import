eea973b2fc5d903cc25eefffcd3e6c2eb11d43fd
Jon Cheng <jon.syndicate@gmail.com>
2020-05-20 09:47:14 -0400

    ignore leading and trailing white space when auto matching fields

--- src/components/VueCsvImport.vue
+++ src/components/VueCsvImport.vue
@@ -280,11 +280,11 @@
                         this.fieldsToMap.forEach(field => {
                             newVal[0].forEach((columnName, index) => {
                                 if(this.autoMatchIgnoreCase === true){
-                                    if(field.key.toLowerCase() === columnName.toLowerCase()){
+                                    if(field.key.toLowerCase().trim() === columnName.toLowerCase().trim()){
                                         this.map[field.key] = index;
                                     }
                                 } else{
-                                    if(field.key === columnName){
+                                    if(field.key.trim() === columnName.trim()){
                                         this.map[field.key] = index;
                                     }
                                 }
